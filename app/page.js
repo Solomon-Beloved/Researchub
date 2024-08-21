ca9fb00f69a04dbe47ee93d8bfbdb9df1447946e
Temitope-Beloved <olafusisam236@gmail.com>
2024-08-21 15:25:29 -0700

    re-inting the project

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import React from 'react'
+
 const page = () => {
   return (
-    <div className='bg-cyan-950 h-screen text-white' >
-      Homepage
+    <div className=" p-4 bg-purple-900 rounded-md shadow-md">
+      homepage
     </div>
   )
 }
